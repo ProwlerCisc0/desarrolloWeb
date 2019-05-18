989584d9aa20484f6af3523a6b3825b14cfeb1ba
ProwlerCisc0 <i.rojel@alumnos.duoc.cl>
2019-05-18 01:30:16 -0400

    agregadoo jquery

--- js/main.js
+++ js/main.js
@@ -21,13 +21,46 @@ document.addEventListener('DOMContentLoaded', function(){
 
         //  Extras
         
-        var camisas = document.getElementById('camisa_evento')
-        var etiquetas = document.getElementById('etiquetas')
+        var camisas = document.getElementById('camisa_evento');
+        var etiquetas = document.getElementById('etiquetas');
 
         calcular.addEventListener('click', calcularMontos);
         pase_dia.addEventListener('blur', mostrarDias);
         pase_dosdias.addEventListener('blur', mostrarDias);
         pase_completo.addEventListener('blur', mostrarDias);
+        nombre.addEventListener('blur',validarCampos);
+        apellido.addEventListener('blur', validarCampos);
+        email.addEventListener('blur', validarMail);
+
+
+        function validarMail(){
+            if(this.value.indexOf("@") > -1){
+                errorDiv.style.display= 'none';
+                this.style.border = '1px solid #cccccc';
+            }
+            else{
+                errorDiv.style.display = 'block';
+                errorDiv.innerHTML = "Debe tener al menos un @";
+                this.style.border = "1px solid red";
+                errorDiv.style.border = '1px solid red';
+            }
+        }
+
+
+        function validarCampos(){
+            if(this.value ==''){
+                errorDiv.style.display='block';
+                errorDiv.innerHTML="Este campo es obligatorio";
+                this.style.border = '1px solid red';
+                errorDiv.style.border = '2px solid red';
+                
+            }
+            else{
+                errorDiv.style.display='none';
+                this.style.border= '1px solid #cccccc';
+            }
+        }
+
 
         function calcularMontos(event){
             event.preventDefault();
@@ -107,3 +140,69 @@ document.addEventListener('DOMContentLoaded', function(){
 
     });//Dom Content Loaded
 })()
+
+
+$(function() {
+
+    var windowHeight =$(window).height();
+    var barraAltura = $('.barra').innerHeight();
+
+
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll > windowHeight){
+            $('.barra').addClass('fixed');
+            $('body').css({'margin-top':barraAltura+'px' });
+        }
+        else{
+            $('.barra').removeClass('fixed');
+            $('body').css({'margin-top': '0px' });
+        }
+    });
+
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+    $('.menu-programa a').on('click', function() {
+
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+        return false;
+    });
+
+
+    var resumenLista = $('.resumen-evento');
+    if(resumenLista.length >0){
+        $('.resumen-evento').waypoint(function(){
+            $('.resumen-evento li:nth-child(1) p').animateNumber({number : 6}, 1600);
+            $('.resumen-evento li:nth-child(2) p').animateNumber({number : 15}, 1600);
+            $('.resumen-evento li:nth-child(3) p').animateNumber({number : 8}, 1600);
+            $('.resumen-evento li:nth-child(4) p').animateNumber({number : 10}, 1600);
+        }, {
+            offset: '50%'
+        });
+    }
+ 
+
+
+
+
+
+    $('.menu-movil').on('click',function(){
+        $('.navegacion-principal').slideToggle();
+    });
+
+
+
+    $('.cuenta-regresiva').countdown('2019/08/16 09:00:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
+
+    
+
+});
